efaf4b09ad569fb0a918fae7bcb097eef77ef62b
v-turin <vladimir.turin@opencascade.com>
2018-11-08 14:39:06 +0300

    Linux build

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,11 +4,14 @@ mkdir build && cd build
 pip install empy
 pip install catkin-pkg
 
-cmake -G "Ninja"                            \
-    -DCMAKE_INSTALL_PREFIX=${PREFIX}        \
-    -DCMAKE_BUILD_TYPE=Release              \
-    -DRT_LIBRARY=${PREFIX}/lib/libmkl_rt.so \
-    -DSETUPTOOLS_DEB_LAYOUT:BOOL=OFF        \
+cmake -G "Ninja"                                                    \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX}                                \
+    -DCMAKE_PREFIX_PATH=${PREFIX}                                   \
+    -DCMAKE_BUILD_TYPE=Release                                      \
+    -DSETUPTOOLS_DEB_LAYOUT:BOOL=OFF                                \
+    -DPYTHON_EXECUTABLE=`which python`                              \
+    -DEMPY_EXECUTABLE=`python -c 'import em; print(em.__file__)'`   \
+    -DCMAKE_LIBRARY_ARCHITECTURE=x86_64-linux-gnu                   \
     ..
 
 ninja install
